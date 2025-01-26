29230e040117912838b0d8604441697f56fde8b1
PaoloVisone <polvis.pv.22@gmail.com>
2025-01-26 22:34:44 +0100

    [feat] Add get call

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ const endopoint = 'https://lanciweb.github.io/demo/api/pictures/';
 
 // Variabile d'appoggio
 let item = ""
+
+// Chiamata get
+axios.get(endopoint)
+    .then(reponse => {
+        const result = reponse.data;
+        console.log(result);
+
+    })
